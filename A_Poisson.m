59efa42fc52e735d8ebc9e9f870bed338e8e44b7
Артём Захарин <artemdnk@gmail.com>
2016-12-26 14:54:17 +0700

    update 2

--- A_Poisson.m
+++ A_Poisson.m
@@ -1,6 +1,6 @@
 function A = A_Poisson( Div, ro, Node )
 % Правая часть уравнения Пуассона
-D = (Node(i,j,k).Div-Node(i,j,k).Div)./abs(Node(i,j,k).t-Node(i,j,k).t);
+D = (Node(i,j).Div-Node(i,j).Div)./abs(Node(i,j).t-Node(i,j).t);
 B1 = 2.*((Node(i,j+1).u-Node(i,j-1).u)./(2.*(abs(Node(i,j-1).y-Node(i,j+1).y))));
 B2 = ((Node(i+1,j).v-Node(i-1,j).v)./(2.*(abs(Node(i-1,j).x-Node(i+1,j).x))));
 C1 = (((Node(i+1,j).u-Node(i-1,j).u)./(2.*(abs(Node(i-1,j).x-Node(i+1,j).x)))).^2);
